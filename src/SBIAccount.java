a57f86af4bf3fc881bbd92c6d71ade86411633c6
Suman Das <sumandas131299@gmail.com>
2023-08-20 21:41:57 +0530

    Banking App SBI update

--- src/SBIAccount.java
+++ src/SBIAccount.java
@@ -1,2 +1,26 @@
-public class SBIAccount {
+public class SBIAccount implements BankAccountInterface{
+    @Override
+    public String fetchBalance(String Password) {
+        return null;
+    }
+
+    @Override
+    public String addMoney(double amount) {
+        return null;
+    }
+
+    @Override
+    public String WithdrawMoney(double amount, String password) {
+        return null;
+    }
+
+    @Override
+    public String ChangePassword(String oldPassword, String newPassword) {
+        return null;
+    }
+
+    @Override
+    public double calculateInterest(int year) {
+        return 0;
+    }
 }
